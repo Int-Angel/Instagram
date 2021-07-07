91d63e061729bb663715d701bf217b47b4d13daf
Int-Angel <Padilla.angel88@gmail.com>
2021-07-07 09:11:28 -0500

    Sign up + Login + User Persistence

--- Instagram/app/src/main/java/com/example/instagram/LoginActivity.java
+++ Instagram/app/src/main/java/com/example/instagram/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.instagram;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,16 +18,19 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
     private final static String TAG = "LoginActivity";
+    private final int REQUEST_CODE = 22;
 
     private Button btnLogin;
+    private Button btnSignUp;
     private EditText etUsername;
     private EditText etPassword;
 
-
+    private ActivityResultLauncher<Intent> singUpLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +38,11 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         if(ParseUser.getCurrentUser() != null){
-
+            goMainActivity();
         }
 
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSingUp);
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
 
@@ -43,6 +52,27 @@ public class LoginActivity extends AppCompatActivity {
                 loginUser(etUsername.getText().toString(), etPassword.getText().toString());
             }
         });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goSignUpActivity();
+            }
+        });
+
+        singUpLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK){
+                            Intent data = result.getData();
+                            Log.i(TAG,data.getExtras().getString("username"));
+                            //Toast.makeText(LoginActivity.this, data.getExtras().getString("username"),Toast.LENGTH_LONG).show();
+                            signUpUser(data.getExtras().getString("username"),data.getExtras().getString("password"));
+                        }
+                    }
+                });
     }
 
     private void loginUser(String username, String password){
@@ -58,6 +88,30 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(LoginActivity.this, "Success login!",Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void signUpUser(String username, String password){
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null){
+                    Log.e(TAG,"Issue with login", e);
+                    Toast.makeText(LoginActivity.this, "Fail SignUp!",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success SignUp!",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goSignUpActivity(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        singUpLauncher.launch(intent);
     }
 
     private void goMainActivity(){
---
+++ Instagram/app/src/main/java/com/example/instagram/SignUpActivity.java
@@ -0,0 +1,53 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignUpActivity";
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        etUsername = findViewById(R.id.etUsernameSU);
+        etPassword = findViewById(R.id.etPasswordSU);
+        btnSignup = findViewById(R.id.btnSingUpSU);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUpUser(etUsername.getText().toString(), etPassword.getText().toString());
+            }
+        });
+
+    }
+
+    private void signUpUser(String username, String password){
+        Intent data = new Intent();
+        data.putExtra("username", username);
+        data.putExtra("password", password);
+        setResult(Activity.RESULT_OK,data);
+        finish();
+    }
+
+}
